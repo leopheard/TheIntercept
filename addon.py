99b9a71351720967b750a5d8ec6667c30f7a2d3d
leopheard <leopheard.misc@gmail.com>
2024-06-21 14:09:25 +0100

    upload

--- addon.py
+++ addon.py
@@ -1,80 +1,58 @@
 from xbmcswift2 import Plugin, xbmcgui
-from resources.lib import theinterceptpodcasts
+from resources.lib import theintercept
 
 plugin = Plugin()
 
-url0 = "http://api.spokenlayer.com/feed/channel/v1-intercept-news2-ext/3c9929b72538c12bd92ac6762f8d798b9d4e8cdca7692ea74f466061d01816cb"
-url1 = "https://rss.prod.firstlook.media/deconstructed/podcast.rss"
-url2 = "https://feeds.feedburner.com/InterceptedWithJeremyScahill"
-url3 = "https://rss.prod.firstlook.media/murderville/podcast.rss"
-url4 = "https://feeds.megaphone.fm/somebody"
-url5 = "https://rss.art19.com/american-isis"
+url1 = "https://feeds.acast.com/public/shows/1d1223a2-9d05-473b-9e79-c2b65b71d676" #DECONSTRUCTED#
+url2 = "https://feeds.acast.com/public/shows/f5b64019-68c3-57d4-b70b-043e63e5cbf6" #INTERCEPTED#
+url3 = "https://feeds.acast.com/public/shows/c66d3932-72a5-49e9-bc88-ca2080d23735" #MURDER GA#
+url4 = "https://www.omnycontent.com/d/playlist/e73c998e-6e60-432f-8610-ae210140c5b1/5a115af8-776f-41c9-bf1b-ae300188d281/d3d0764f-3b72-4554-b783-ae300188d28f/podcast.rss" #SOMEBODY#
 
 @plugin.route('/')
 def main_menu():
     items = [
-#        {
-#            'label': plugin.get_string(30000), 
-#            'path': plugin.url_for('spoken_edition'),
-#            'thumbnail': "https://github.com/leopheard/thedeprogram/blob/master/resources/media/icon.jpg?raw=true"},
         {
             'label': plugin.get_string(30001), 
             'path': plugin.url_for('all_deconstructed'),
-            'thumbnail': "https://github.com/leopheard/thedeprogram/blob/master/resources/media/2.jpg?raw=true"},
+            'thumbnail': "https://github.com/leopheard/TheIntercept/blob/master/resources/media/1.jpg?raw=true"},
         {
             'label': plugin.get_string(30002), 
             'path': plugin.url_for('all_intercepted'),
-            'thumbnail': "https://github.com/leopheard/thedeprogram/blob/master/resources/media/1.jpg?raw=true"},
+            'thumbnail': "https://github.com/leopheard/TheIntercept/blob/master/resources/media/2.jpg?raw=true"},
 {
             'label': plugin.get_string(30003), 
             'path': plugin.url_for('all_murderGA'),
-            'thumbnail': "https://github.com/leopheard/thedeprogram/blob/master/resources/media/3.jpg?raw=true"},
+            'thumbnail': "https://github.com/leopheard/TheIntercept/blob/master/resources/media/3.jpg?raw=true"},
 {
             'label': plugin.get_string(30004), 
             'path': plugin.url_for('all_somebody'),
-            'thumbnail': "https://github.com/leopheard/thedeprogram/blob/master/resources/media/4.jpg?raw=true"},
-{
-            'label': plugin.get_string(30005), 
-            'path': plugin.url_for('all_americanisis'),
-            'thumbnail': "https://content.production.cdn.art19.com/images/a3/dc/6f/f7/a3dc6ff7-5f36-445f-a563-f8adaf8d35e3/ae4e3bdb54d33f6bd9f9825359c3ff897014a2e903d12922f92e1b55ec934f9e2a285e3afff5ef27748f0517095f69683c8bac4ee1e4498b5913b5b880ab4897.jpeg"},
+            'thumbnail': "https://github.com/leopheard/TheIntercept/blob/master/resources/media/4.jpg?raw=true"},
     ]
     return items
 
-#@plugin.route('/spoken_edition/')
-#def spoken_edition():
-#    soup0 = theinterceptpodcasts.get_soup0(url0)
-#    playable_podcast0 = theinterceptpodcasts.get_playable_podcast0(soup0)
-#    items = theinterceptpodcasts.compile_playable_podcast0(playable_podcast0)
-#    return items
 @plugin.route('/all_deconstucted/')
 def all_deconstructed():
-    soup1 = theinterceptpodcasts.get_soup1(url1)
-    playable_deconstructed = theinterceptpodcasts.get_playable_deconstructed(soup1)
-    items = theinterceptpodcasts.compile_playable_deconstructed(playable_deconstructed)
+    soup1 = theintercept.get_soup1(url1)
+    playable_deconstructed = theintercept.get_playable_deconstructed(soup1)
+    items = theintercept.compile_playable_deconstructed(playable_deconstructed)
     return items
 @plugin.route('/all_intercepted/')
 def all_intercepted():
-    soup2 = theinterceptpodcasts.get_soup2(url2)
-    playable_intercepted = theinterceptpodcasts.get_playable_intercepted(soup2)
-    items = theinterceptpodcasts.compile_playable_intercepted(playable_intercepted)
+    soup2 = theintercept.get_soup2(url2)
+    playable_intercepted = theintercept.get_playable_intercepted(soup2)
+    items = theintercept.compile_playable_intercepted(playable_intercepted)
     return items
 @plugin.route('/all_murderGA/')
 def all_murderGA():
-    soup3 = theinterceptpodcasts.get_soup3(url3)
-    playable_murderGA = theinterceptpodcasts.get_playable_murderGA(soup3)
-    items = theinterceptpodcasts.compile_playable_murderGA(playable_murderGA)
+    soup3 = theintercept.get_soup3(url3)
+    playable_murderGA = theintercept.get_playable_murderGA(soup3)
+    items = theintercept.compile_playable_murderGA(playable_murderGA)
     return items
 @plugin.route('/all_somebody/')
 def all_somebody():
-    soup4 = theinterceptpodcasts.get_soup4(url4)
-    playable_somebody = theinterceptpodcasts.get_playable_somebody(soup4)
-    items = theinterceptpodcasts.compile_playable_somebody(playable_somebody)
-    return items
-@plugin.route('/all_americanisis/')
-def all_americanisis():
-    soup5 = theinterceptpodcasts.get_soup5(url5)
-    playable_americanisis = theinterceptpodcasts.get_playable_americanisis(soup5)
-    items = theinterceptpodcasts.compile_playable_americanisis(playable_americanisis)
+    soup4 = theintercept.get_soup4(url4)
+    playable_somebody = theintercept.get_playable_somebody(soup4)
+    items = theintercept.compile_playable_somebody(playable_somebody)
     return items
 if __name__ == '__main__':
     plugin.run()
--- resources/lib/theintercept.py
+++ resources/lib/theintercept.py
@@ -2,11 +2,6 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-def get_soup0(url0):
-    page = requests.get(url0)
-    soup0 = BeautifulSoup(page.text, 'html.parser')
-    print("type: ", type(soup0))
-    return soup0
 def get_soup1(url1):
     page = requests.get(url1)
     soup1 = BeautifulSoup(page.text, 'html.parser')
@@ -27,42 +22,6 @@ def get_soup4(url4):
     soup4 = BeautifulSoup(page.text, 'html.parser')
     print("type: ", type(soup4))
     return soup4
-def get_soup5(url5):
-    page = requests.get(url5)
-    soup5 = BeautifulSoup(page.text, 'html.parser')
-    print("type: ", type(soup5))
-    return soup5
-
-def get_playable_podcast0(soup):
-    subjects = []
-    for content in soup.find_all('item'):
-        try:        
-            link = content.find('enclosure')
-            link = link.get('url')
-            print("\n\nLink: ", link)
-            title = content.find('title')
-            title = title.get_text()
-            thumbnail = content.find('itunes:image')
-            thumbnail = thumbnail.get('href')
-        except AttributeError:
-            continue
-        item = {
-                'url': link,
-                'title': title,
-                'thumbnail': thumbnail,
-        }
-        subjects.append(item)
-    return subjects
-def compile_playable_podcast0(playable_podcast0):
-    items = []
-    for podcast in playable_podcast0:
-        items.append({
-            'label': podcast['title'],
-            'thumbnail': podcast['thumbnail'],
-            'path': podcast['url'],
-            'is_playable': True,
-    })
-    return items
 
 def get_playable_podcast01(soup1):
     subjects = []
@@ -75,11 +34,14 @@ def get_playable_podcast01(soup1):
             title = title.get_text()
             thumbnail = content.find('itunes:image')
             thumbnail = thumbnail.get('href')
+            desc = content.find('itunes:subtitle')
+            desc = desc.get_text()
         except AttributeError:
             continue
         item = {
                 'url': link,
                 'title': title,
+		'description': desc,
                 'thumbnail': thumbnail,
         }
         subjects.append(item)
@@ -91,9 +53,11 @@ def compile_playable_podcast01(playable_podcast01):
             'label': podcast['title'],
             'thumbnail': podcast['thumbnail'],
             'path': podcast['url'],
+            'info': {'plot': podcast['description']},
             'is_playable': True,
     })
     return items
+
 def get_playable_deconstructed(soup1):
     subjects = []
     for content in soup1.find_all('item'):
@@ -103,16 +67,17 @@ def get_playable_deconstructed(soup1):
             print("\n\nLink: ", link)
             title = content.find('title')
             title = title.get_text()
-#            thumbnail = content.find('itunes:image')
-#            thumbnail = thumbnail.get('href')
-#            desc = content.find('itunes:subtitle')
-#            desc = desc.get_text()
+            thumbnail = content.find('itunes:image')
+            thumbnail = thumbnail.get('href')
+            desc = content.find('itunes:subtitle')
+            desc = desc.get_text()
         except AttributeError:
             continue
         item = {
                 'url': link,
                 'title': title,
-                'thumbnail': "https://github.com/leopheard/thedeprogram/blob/master/resources/media/2.jpg?raw=true",
+		'description': desc,
+                'thumbnail': thumbnail,
         }
         subjects.append(item) 
     return subjects
@@ -122,6 +87,7 @@ def compile_playable_deconstructed(playable_deconstructed):
         items.append({
             'label': podcast['title'],
             'thumbnail': podcast['thumbnail'],
+            'info': {'plot': podcast['description']},
             'path': podcast['url'],
             'is_playable': True,
     })
@@ -136,14 +102,14 @@ def get_playable_intercepted(soup2):
             print("\n\nLink: ", link)
             title = content.find('itunes:title')
             title = title.get_text()
-#            thumbnail = content.find('itunes:image')
-#            thumbnail = thumbnail.get('href')
+            thumbnail = content.find('itunes:image')
+            thumbnail = thumbnail.get('href')
         except AttributeError:
             continue
         item = {
                 'url': link,
                 'title': title,
-                'thumbnail': "https://github.com/leopheard/thedeprogram/blob/master/resources/media/1.jpg?raw=true",
+                'thumbnail': thumbnail,
         }
         subjects.append(item) 
     return subjects
@@ -210,37 +176,6 @@ def get_playable_somebody(soup4):
         subjects.append(item)
     return subjects
 def compile_playable_somebody(playable_somebody):
-    items = []
-    for podcast in playable_somebody:
-        items.append({
-            'label': podcast['title'],
-            'thumbnail': podcast['thumbnail'],
-            'path': podcast['url'],
-            'is_playable': True,
-    })
-    return items
-
-def get_playable_americanisis5(soup5):
-    subjects = []
-    for content in soup5.find_all('item'):
-        try:        
-            link = content.find('enclosure')
-            link = link.get('url')
-            print("\n\nLink: ", link)
-            title = content.find('title')
-            title = title.get_text()
-            thumbnail = content.find('itunes:image')
-            thumbnail = thumbnail.get('href')
-        except AttributeError:
-            continue
-        item = {
-                'url': link,
-                'title': title,
-                'thumbnail': thumbnail,
-        }
-        subjects.append(item)
-    return subjects
-def compile_playable_americanisis(playable_americanisis):
     items = []
     for podcast in playable_somebody:
         items.append({
